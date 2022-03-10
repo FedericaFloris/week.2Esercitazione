6547c607103f7d691a8661924f20bcf65c78476b
FedericaFloris <feflo92@gmail.com>
2022-03-10 15:58:28 +0100

    primocommit

---
+++ Week.2Esercitazione/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week._2Esercitazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
